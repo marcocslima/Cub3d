1236a8d3d9b03bbd98659ad9eb454d010f3e66d4
marcocslima <marcocslima@gmail.com>
2023-01-27 07:32:55 +0100

    feature/parse_input 27/1 3:30

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: mcesar-d <mcesar-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 23:16:02 by mcesar-d          #+#    #+#             */
-/*   Updated: 2023/01/27 06:59:51 by mcesar-d         ###   ########.fr       */
+/*   Updated: 2023/01/27 07:30:34 by mcesar-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ void	print_exit(char *input)
 	exit (1);
 }
 
-char *check_input(int argc, char **argv)
+int	check_input(int argc, char **argv)
 {
 	int		len_entry;
+	int		fd;
 	char	*is_cub;
 
 	len_entry = ft_strlen(argv[1]);
-	
 	if(argc != 2)
 		print_exit("Error: wrong number of arguments");
 	is_cub = ft_substr(argv[1], (len_entry - 4), 5);
@@ -34,14 +34,24 @@ char *check_input(int argc, char **argv)
 		print_exit("Error: map file needs to be .cub");
 	}
 	free(is_cub);
-	return (argv[1]);
+	fd = open(argv[1], O_DIRECTORY);
+	if (fd != -1)
+		print_exit("Error: file can´t be a directory");
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		print_exit("Error: map can´t be found");
+	return (fd);
 }
 
 int	main(int argc, char *argv[])
 {
-	char *map;
+	int		map;
+	char 	*line;
 	
 	map  = check_input(argc, argv);
-	printf("%s\n", map);
+	line = get_next_line(map);
+	printf("%s\n", line);
+	free(line);
+	close(map);
 	return (0);
 }
