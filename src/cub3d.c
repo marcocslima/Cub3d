434881716072071220c8d83d5ff2ed06ed40e0d0
Alice Akaki <akakialice@gmail.com>
2023-03-03 13:24:33 -0400

    feat: use mlx images to draw

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: alida-si <alida-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 23:16:02 by mcesar-d          #+#    #+#             */
-/*   Updated: 2023/03/02 11:59:50 by alida-si         ###   ########.fr       */
+/*   Updated: 2023/03/03 13:22:37 by alida-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,25 @@ int	handle_events(int key_code, t_game *game)
 	return (1);
 }
 
+void	img_pix_put(t_img *img, int x, int y, int color)
+{
+	char	*pixel;
+	int		i;
+
+	i = img->bpp - 8;
+    pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	while (i >= 0)
+	{
+		/* big endian, MSB is the leftmost bit */
+		if (img->endian != 0)
+			*pixel++ = (color >> i) & 0xFF;
+		/* little endian, LSB is the leftmost bit */
+		else
+			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+		i -= 8;
+	}
+}
+
 int	render_rect(t_game *game)
 {
 	int	i;
@@ -60,8 +79,7 @@ int	render_rect(t_game *game)
 	{
 		j = 250;
 		while (j < 250 + 8)
-			mlx_pixel_put(game->mlx_data->mlx_ptr, game->mlx_data->mlx_win,
-				j++, i, 0xFF0000);
+			img_pix_put(game->img, j++, i, 0xFF0000);
 		++i;
 	}
 	return (0);
@@ -69,11 +87,8 @@ int	render_rect(t_game *game)
 
 int	render(t_game *game)
 {
-	//if window has been destroyed, we don't want to put the pixel !
-	/*if (game->mlx_win != NULL)
-		mlx_pixel_put(game->mlx_ptr, game->mlx_win, 500/2, 500/2, 0xFF0000);
-	return (0);*/
 	render_rect(game);
+	mlx_put_image_to_window(game->mlx_data->mlx_ptr, game->mlx_data->mlx_win, game->img->mlx_img, 0, 0);
 	return (1);
 }
 
@@ -92,6 +107,10 @@ int	main(int argc, char *argv[])
 		check_header(&game);
 		verify_map(&game);
 		//print_whole_map(game);
+
+		game->img->mlx_img = mlx_new_image(game->mlx_data->mlx_ptr, 500, 500);
+		game->img->addr = mlx_get_data_addr(game->img->mlx_img, &game->img->bpp, &game->img->line_len, &game->img->endian);
+
 		mlx_loop_hook(game->mlx_data->mlx_ptr, &render, game);
 		mlx_hook(game->mlx_data->mlx_win, 17, 1L << 17, close_window, game);
 		mlx_key_hook(game->mlx_data->mlx_win, handle_events, game);
