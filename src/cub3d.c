e83ede4212bbe4189619f00b62c657d4ce43685b
Alice Akaki <akakialice@gmail.com>
2023-03-09 10:09:28 -0400

    fix: render problem

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: alida-si <alida-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 23:16:02 by mcesar-d          #+#    #+#             */
-/*   Updated: 2023/03/08 21:41:39 by alida-si         ###   ########.fr       */
+/*   Updated: 2023/03/09 10:07:43 by alida-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,40 +32,6 @@ void	read_file(int fd, t_game **game)
 	free(ret);
 }
 
-/*void	plot_map(t_game *game)
-{
-	int	i;
-	int	j;
-	int	color;
-	int	side;
-
-	i = -1;
-	//side = data->l_side;
-	while(++i < game->map->map_higth)
-	{
-		j = -1;
-		while(++j < game->map->map_width)
-		{
-			color = COLOR_BLACK;
-			if(game->map->map[i][j] == '1')
-				color = COLOR_RED;
-			if(game->map->map[i][j] == '0' || game->map->map[i][j] == 'N')
-				color = COLOR_YELLOW;
-			render_rect(game, (t_rect){j * side, i * side, side, side, color});
-			render_rect(&data->img, (t_rect){data->game->player.pos[0] * side, data->gm->player.pos[1] * side, 5, 5, RED_PIXEL});
-			//render_rect(&data->img, (t_rect){data->gm->player.pos[0] * side + data->gm->player.dir[0], data->gm->player.pos[1] * side + data->gm->player.dir[1], 4, 4, BLUE_PIXEL});
-			float d = 0.1;
-			while(d < 1)
-			{
-				render_rect(&data->img, (t_rect){data->gm->player.pos[0] * side + data->gm->player.dir[0] * d, data->gm->player.pos[1] * side + data->gm->player.dir[1] * d, 2, 2, BLUE_PIXEL});
-				d = d + 0.02;
-			}
-			//render_rect(&data->img, (t_rect){j * side, i * side, 1, side, BLUE_SKY_PIXEL});
-			//render_rect(&data->img, (t_rect){j * side, i * side, side, 1, BLUE_SKY_PIXEL});
-		}
-	}
-}*/
-
 int	handle_events(int key_code, t_game *game)
 {
 	if (key_code == 65307)
@@ -78,7 +44,6 @@ int	handle_events(int key_code, t_game *game)
 		*&game->img_data->x_position -= 5;
 	if (key_code == D)
 		*&game->img_data->x_position += 5;
-	render(game);
 	return (1);
 }
 
