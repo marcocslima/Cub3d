6c9656765cb9295b1aef2b77642dadaa3013198c
Alice Akaki <akakialice@gmail.com>
2023-03-29 11:15:11 -0400

    refactor: check_vertical_hit_wall_left function

--- src/render_img.c
+++ src/render_img.c
@@ -6,7 +6,7 @@
 /*   By: alida-si <alida-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 16:03:17 by alida-si          #+#    #+#             */
-/*   Updated: 2023/03/29 11:04:45 by alida-si         ###   ########.fr       */
+/*   Updated: 2023/03/29 11:09:06 by alida-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -171,42 +171,18 @@ int	distance_to_vertical_wall_right(t_game *game)
 
 int	check_vertical_hit_wall_left(t_game *game)
 {
-	int	ay;
-	int	ax;
-	int	d_y;
-	int	d_x;
-	int	hipotenusa;
+	int	y_coordinate;
+	int	x_coordinate;
+	int	x_step;
+	int	y_step;
+	int	ray_length;
 
-	//find first vertical intersection
-	ax = game->player->x_position / 40;
-	ax *= 40;
-	ax -= 1;
-	ay = (game->player->x_position - ax) * ( -tan (game->player->angle));
-	ay += game->player->y_position;
-
-
-	int x_step = -40;
-	int y_step = 40 * -tan(game->player->angle);
-	int bx= ax+x_step;
-	int by= ay+y_step;
-
-	d_y = game->player->y_position - ay;
-	d_x = game->player->x_position - ax;
-	hipotenusa = sqrt(pow(d_y, 2) + pow(d_x, 2));
-	int cel_x = ax/40;
-	int cel_y = ay/40;
-	while (cel_x >= 0 && cel_x < game->map->map_width
-			&& cel_y >= 0 && cel_y < game->map->map_higth && game->map->map[ay/40][ax/40] != '1')
-	{
-		ay += y_step;
-		ax += x_step;
-		d_y = game->player->y_position - ay;
-		d_x = game->player->x_position - ax;
-		hipotenusa = sqrt(pow(d_y, 2) + pow(d_x, 2));
-		cel_x = ax/40;
-		cel_y = ay/40;
-	}
-	return (hipotenusa);
+	x_coordinate = ((int)game->player->x_position / 40) * 40 - 1;
+	y_coordinate = (int)(game->player->x_position - x_coordinate) * ( -tan (game->player->angle)) + game->player->y_position;
+	x_step = -40;
+	y_step = 40 * -tan(game->player->angle);
+	ray_length = hit_wall(game, y_coordinate, x_coordinate, y_step, x_step);
+	return (ray_length);
 }
 
 int	looking_up(float angle)
